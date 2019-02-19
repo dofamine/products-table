0fb4a4bc2d06b97bf398f441fcc14923d45170d6
Oleg <pryatko.oleg@gmail.com>
2019-02-19 10:53:43 +0200

    init base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductFiltersComponent } from './features/product-filters/product-filters.component';
+import { ProductTableComponent } from './features/product-table/product-table.component';
+import { ProductDetailsComponent } from './features/product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductFiltersComponent,
+    ProductTableComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/features/product-filters/product-filters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-filters',
+  templateUrl: './product-filters.component.html',
+  styleUrls: ['./product-filters.component.scss']
+})
+export class ProductFiltersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
