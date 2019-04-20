992c0d21c8536ab86cb9a0edb999683ad162967a
Oleh Priatko <pryatko.oleg@gmail.com>
2019-04-20 21:33:17 +0300

    investigate around switchMap, and difference between take(), takeUntil(), and add(), all of these work the same looks like,

--- src/app/features/product-table/product-table.component.ts
+++ src/app/features/product-table/product-table.component.ts
@@ -1,8 +1,8 @@
 import {Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {BehaviorSubject, combineLatest, Observable} from 'rxjs';
+import {BehaviorSubject, combineLatest, Observable, Subject, Subscriber, Subscription} from 'rxjs';
 import {User} from './User';
 import {DataService} from '../data.service';
-import {map, tap} from 'rxjs/operators';
+import {map, switchMap, take, takeUntil, takeWhile, tap} from 'rxjs/operators';
 import {ProductFiltersComponent} from '../product-filters/product-filters.component';
 
 @Component({
@@ -12,22 +12,29 @@ import {ProductFiltersComponent} from '../product-filters/product-filters.compon
 })
 export class ProductTableComponent implements OnInit, OnDestroy {
   users: Observable<User[]>;
-  search$: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  search$: BehaviorSubject<any> = new BehaviorSubject<any>('');
   @Input('searchingStream') searching$: Observable<string>;
   @ViewChild(ProductFiltersComponent) filter: ProductFiltersComponent;
+  sub;
+  subb = new Subscription();
+  subb1 = new Subscriber((value => console.log(value)));
 
   constructor(private data: DataService) {
   }
 
   ngOnInit() {
-    this.users = combineLatest(this.data.users, this.searching$, this.search$)
-      .pipe(
-        tap((data) => console.log(data)),
-        map(([users, search, s]: [User[], string, string]) =>
-          users.filter((user) => user.name.includes(search) && user.name.includes(s))
-        )
-      );
-
+    // this.users = combineLatest(this.data.users, this.searching$, this.search$)
+    //   .pipe(
+    //     tap((data) => console.log(data)),
+    //     map(([users, search, s]: [User[], string, string]) =>
+    //       users.filter((user) => user.name.includes(search) && user.name.includes(s))
+    //     )
+    this.users = this.data.users;
+    //   );
+    let s;
+    s = this.data.users.pipe(switchMap(() => this.search$), take(5)).subscribe(data => console.log('switchMap', s));
+    this.search$.subscribe(data => console.log('Search', data));
+    this.users.subscribe(data => console.log('Users', data));
   }
 
   getData() {
@@ -39,6 +46,10 @@ export class ProductTableComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    console.log(this.searching$);
+    this.subb.unsubscribe();
+    // this.search$.complete();
+    console.log(this.subb);
+    console.log('========');
+    console.log(this.sub);
   }
 }
